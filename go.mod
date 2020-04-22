f4c8b51d51b988c225f9ea6abd4274bfa66f75f0
Sunao Uehara <uehara.sunao@gmail.com>
2020-04-22 00:46:53 -0700

    use v.1.0.0 on master

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sunao-uehara/go-sandbox
+
+go 1.14
+
+require github.com/sunao-uehara/go-modules v1.0.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	modtest "github.com/sunao-uehara/go-modules"
+)
+
+func main() {
+	modtest.Print()
+}
